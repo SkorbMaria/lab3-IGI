94449c13fbdaf055cacc6ac9e298c45c9d83e63e
SkorbMaria <48706471+SkorbMaria@users.noreply.github.com>
2019-04-14 21:58:59 +0300

    Add files via upload

---
+++ CacheLastMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using lab1_ef;
+
+namespace lab3_igi
+{
+    public class CacheLastMiddleware
+    {
+        readonly RequestDelegate _next;
+        readonly IMemoryCache _memoryCache;
+
+        public CacheLastMiddleware(RequestDelegate next, IMemoryCache memCache)
+        {
+            this._next = next;
+            this._memoryCache = memCache;
+        }
+
+        public async Task Invoke(HttpContext context, BibliotekaContext db)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            object model = null;
+            
+            switch (path)
+            {
+                case "/gr":
+                    model = db.Rooms.Last(); 
+                    break;
+                case "/gst":
+                    model = db.ServiceTypes.Last();
+                    break;
+                case "/gemp":
+                    model = db.Employees.Last();
+                    break;
+            }
+
+            var modelTemp = model;
+
+            if (!_memoryCache.TryGetValue(path, out modelTemp))
+            {
+                _memoryCache.Set(path, model,
+                    new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(2*17+240)));
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class DbCacheExtensions
+    {
+        public static IApplicationBuilder UseCacheLastFeauture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CacheLastMiddleware>();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using lab3_igi;
+using lab1_ef;
+
+namespace lab2_igi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<BibliotekaContext>();
+            services.AddMemoryCache();
+            services.AddResponseCaching();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("Caching",
+                    new CacheProfile()
+                    {
+                        Duration = 2 * 17 + 240,
+                        Location = ResponseCacheLocation.Any
+                    });
+
+                options.CacheProfiles.Add("NoCaching",
+                    new CacheProfile()
+                    {
+                        Location = ResponseCacheLocation.None,
+                        NoStore = true
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCacheLastFeauture();
+
+            app.UseSession();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = context =>
+                {
+                    context.Context.Response.Headers.Add("Cache-Control", "public, max-age = 2*17+240");
+                }
+            });
+
+            app.UseResponseCaching();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
